ca2c50a3735e1a69241090d89b41a564cde3d03a
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-11-05 12:49:13 +1300

    with fixed index file

--- pages/Users.js
+++ pages/Users.js
@@ -1,29 +1,35 @@
-import {useEffect, useState, useContext} from 'react'; 
-import {UserContext} from './UserContext.js';
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "./UserContext.js";
 
 function Users() {
-    const [listOfUsers, setListOfUsers] = useState([]);
-    function getPosts() {
-        fetch('https://jsonplaceholder.typicode.com/Users')
-        .then(response => response.json())
-        .then(json => setListOfUsers(json))
-        .catch(error => console.log(error));
-    }
-  useEffect (( ) => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  function getPosts() {
+    fetch("https://jsonplaceholder.typicode.com/Users")
+      .then((response) => response.json())
+      .then((json) => setListOfUsers(json))
+      .catch((error) => console.log(error));
+  }
+  useEffect(() => {
     getPosts();
   }, []);
   // const userContext = useContext(UserContext);
-  const {userId, setUserId} = useContext(UserContext);
+  const { userId, setUserId } = useContext(UserContext);
 
   return (
-      <div>
-        <ul>
-            {listOfUsers.map(u => <li key={u.userId}>
-              <span> {u.name} </span>
-              <span> <button onClick = {() => setUserId(u.id) }> get Posts </button> </span>
-              </li>)}
-        </ul>
-    </div>);
+    <div>
+      <ul>
+        {listOfUsers.map((u) => (
+          <li key={u.userId}>
+            <span> {u.name} </span>
+            <span>
+              {" "}
+              <button onClick={() => setUserId(u.id)}> get Posts </button>{" "}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Users;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Posts from "./Posts";
+import Users from "./Users";
+import { UserContext } from "./UserContext";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <UserContext>
+        <Users />
+        <Posts />
+      </UserContext>
     </div>
-  )
+  );
 }
