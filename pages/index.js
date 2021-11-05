095892825bb49891cb787553dc66c7b8c7b713e3
vorad1 <64877335+vorad1@users.noreply.github.com>
2021-11-05 13:28:21 +1300

    Working code without css

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,16 @@ import styles from "../styles/Home.module.css";
 import Posts from "./Posts";
 import Users from "./Users";
 import { UserContext } from "./UserContext";
+import { useState } from "react";
 
 export default function Home() {
+  const [userId,setUserId] = useState(1);
   return (
     <div className={styles.container}>
-      <UserContext>
+      <UserContext.Provider value = {{userId,setUserId}} >
         <Users />
         <Posts />
-      </UserContext>
+      </UserContext.Provider>
     </div>
   );
 }
